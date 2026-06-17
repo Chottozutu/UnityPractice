10871bf58a07bc18bc66b8bae6c1f41251fa2719
StepByStep-cpp <tondemoneeyaa@gmail.com>
2026-06-17 13:29:40 +0900

    Add mouse look

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,13 +6,40 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-        Debug.Log("Update");
+        Move();
+        LookAtMouse();
+    }
 
+    void Move()
+    {
         float x = Input.GetAxisRaw("Horizontal");
         float z = Input.GetAxisRaw("Vertical");
 
         Vector3 move = new Vector3(x, 0, z);
 
         transform.position += move.normalized * moveSpeed * Time.deltaTime;
+    }
+
+    void LookAtMouse()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        Plane plane = new Plane(Vector3.up, Vector3.zero);
+
+        float distance;
+
+        if (plane.Raycast(ray, out distance))
+        {
+            Vector3 targetPoint = ray.GetPoint(distance);
+
+            Vector3 direction = targetPoint - transform.position;
+
+            direction.y = 0;
+
+            if (direction.sqrMagnitude > 0.001f)
+            {
+                transform.rotation = Quaternion.LookRotation(direction);
+            }
+        }
     }
 }
